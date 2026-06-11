383b489d956091b729782c6bcae902da25798945
BananaPB <bananapb@pm.me>
2026-06-11 23:53:12 +0200

    Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://baroudeurculinaire.fr/',
+	site: 'https://bananapb.github.io',
+	base: '/baroudeur-culinaire',
 	integrations: [
 		sitemap({
 		// configuration options
